750f656b377600a0eb6dbc296ced462ae9bffa91
bpinedaf <bpinedaf@gmail.com>
2025-10-15 22:52:53 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Config GitHub/Netlify
 window.CONFIG = {
-  API: '' // p.ej. 'https://script.google.com/macros/s/AKfyc.../exec?path=products'
+  API: 'https://script.google.com/macros/s/AKfycbxUOVcn6K5anNZohvzxtRTPVSvZfyceH21AiBiMbLU4CrgfQ7c5CDp-SKNZK7UN1sHR/exec?path=products' // p.ej. 'https://script.google.com/macros/s/AKfyc.../exec?path=products'
 };
