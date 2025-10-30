4f8afccf35609da984df3b7f864779ac4470a3ef
bpinedaf <bpinedaf@gmail.com>
2025-10-30 09:28:29 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // Config GitHub/Netlify
 window.CONFIG = {
-  API: 'https://script.google.com/macros/s/AKfycbxUOVcn6K5anNZohvzxtRTPVSvZfyceH21AiBiMbLU4CrgfQ7c5CDp-SKNZK7UN1sHR/exec' // p.ej. 'https://script.google.com/macros/s/AKfyc.../exec?path=products'
+  //API: 'https://script.google.com/macros/s/AKfycbxUOVcn6K5anNZohvzxtRTPVSvZfyceH21AiBiMbLU4CrgfQ7c5CDp-SKNZK7UN1sHR/exec' // p.ej. 'https://script.google.com/macros/s/AKfyc.../exec?path=products'
+  API: 'https://script.google.com/macros/s/AKfycbylWQu1v_zy6mCpatSFlpxt5wQICtm8hFrR4EQHfSKVfnCf-HLpw3JXvpGn8Gr7_h6_1w/exec' // This is the new one
 };
